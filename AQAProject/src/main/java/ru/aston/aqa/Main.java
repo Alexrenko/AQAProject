15b88743acc8b8f4ecd4b76d9493053b6ef0dd0b
Alex Renko <alexrenko@yahoo.com>
2023-11-14 00:29:14 +0100

    Lesson_3

--- AQAProject/src/main/java/ru/aston/aqa/Main.java
+++ AQAProject/src/main/java/ru/aston/aqa/Main.java
@@ -1,52 +1,100 @@
 package ru.aston.aqa;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
+        System.out.println(checkSumInRange(7, 25));
+        printStringNTimes("Hello", 3);
+        checkPositivity(0);
+        System.out.println(isNegative(-5));
+        System.out.println(isLeapYear(2020));
+        System.out.println(Arrays.toString(invertArray(new int[]{1, 1, 0, 0, 1, 0, 1, 1, 0, 0})));
+        System.out.println(Arrays.toString(fillArray()));
+        System.out.println(Arrays.toString(doubleIfLessThanSix(new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1})));
+        printMatrix(fillDiagonals());
+        System.out.println(Arrays.toString(createArrayWithInitialValue(5, 3)));
+
     }
 
-    public static void printThreeWords() {
-        System.out.println("Orange");
-        System.out.println("Banana");
-        System.out.println("Apple");
+    public static boolean checkSumInRange(int a, int b) {
+        int sum = a + b;
+        return sum >= 10 && sum <= 20;
     }
 
-    public static void checkSumSign() {
-        int a = 5;
-        int b = -3;
-
-        if (a + b >= 0) {
-            System.out.println("Сумма положительная");
-        } else {
-            System.out.println("Сумма отрицательная");
+    public static void printStringNTimes(String str, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(str);
         }
     }
 
-    public static void printColor() {
-        int value = 101;
-
-        if (value <= 0) {
-            System.out.println("Красный");
-        } else if (value > 0 && value <= 100) {
-            System.out.println("Желтый");
+    public static void checkPositivity(int number) {
+        if (number >= 0) {
+            System.out.println("Число положительное");
         } else {
-            System.out.println("Зеленый");
+            System.out.println("Число отрицательное");
         }
     }
 
-    public static void compareNumbers() {
-        int a = 7;
-        int b = 9;
+    public static boolean isNegative(int number) {
+        return number < 0;
+    }
 
-        if (a >= b) {
-            System.out.println("a >= b");
-        } else {
-            System.out.println("a < b");
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) return true;
+        else if (year % 100 == 0) return false;
+        else return year % 4 == 0;
+    }
+
+    public static int[] invertArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array[i] == 0 ? 1 : 0;
+        }
+        return array;
+    }
+
+    public static int[] fillArray() {
+        int[] array = new int[100];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+        return array;
+    }
+
+    public static int[] doubleIfLessThanSix(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 6) {
+                array[i] *= 2;
+            }
+        }
+        return array;
+    }
+
+    public static int[][] fillDiagonals() {
+        int size = 5;
+        int[][] array = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            array[i][i] = 1;
+            array[i][size - 1 - i] = 1;
+        }
+        return array;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int element : row) {
+                System.out.print(element + "  ");
+            }
+            System.out.println();
         }
     }
+
+    public static int[] createArrayWithInitialValue(int len, int initialValue) {
+        int[] array = new int[len];
+        Arrays.fill(array, initialValue);
+        return array;
+    }
+
 }
 
