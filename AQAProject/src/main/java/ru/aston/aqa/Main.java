e100f125d3bd2e6a93ce4e9e535c352e939c9f49
Alex Renko <alexrenko@yahoo.com>
2023-11-20 17:09:55 +0100

    Lesson_4

--- AQAProject/src/main/java/ru/aston/aqa/Main.java
+++ AQAProject/src/main/java/ru/aston/aqa/Main.java
@@ -5,96 +5,33 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(checkSumInRange(7, 25));
-        printStringNTimes("Hello", 3);
-        checkPositivity(0);
-        System.out.println(isNegative(-5));
-        System.out.println(isLeapYear(2020));
-        System.out.println(Arrays.toString(invertArray(new int[]{1, 1, 0, 0, 1, 0, 1, 1, 0, 0})));
-        System.out.println(Arrays.toString(fillArray()));
-        System.out.println(Arrays.toString(doubleIfLessThanSix(new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1})));
-        printMatrix(fillDiagonals());
-        System.out.println(Arrays.toString(createArrayWithInitialValue(5, 3)));
 
-    }
-
-    public static boolean checkSumInRange(int a, int b) {
-        int sum = a + b;
-        return sum >= 10 && sum <= 20;
-    }
-
-    public static void printStringNTimes(String str, int n) {
-        for (int i = 0; i < n; i++) {
-            System.out.println(str);
-        }
-    }
-
-    public static void checkPositivity(int number) {
-        if (number >= 0) {
-            System.out.println("Число положительное");
-        } else {
-            System.out.println("Число отрицательное");
-        }
-    }
-
-    public static boolean isNegative(int number) {
-        return number < 0;
-    }
-
-    public static boolean isLeapYear(int year) {
-        if (year % 400 == 0) return true;
-        else if (year % 100 == 0) return false;
-        else return year % 4 == 0;
-    }
-
-    public static int[] invertArray(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            array[i] = array[i] == 0 ? 1 : 0;
-        }
-        return array;
-    }
-
-    public static int[] fillArray() {
-        int[] array = new int[100];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = i + 1;
-        }
-        return array;
-    }
-
-    public static int[] doubleIfLessThanSix(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < 6) {
-                array[i] *= 2;
+        Employee[] employees = createFiveEmployees();
+        for (Employee e : employees) {
+            if (e.getAge() > 40) {
+                e.printInfo();
             }
         }
-        return array;
+
+        Park park = new Park("Диво остров");
+        park.addAttraction("Колесо обозрения", "10:00-22:00", 150);
+        park.addAttraction("Американские горки", "10:00-20:00", 300);
+        park.printParkInfo();
     }
 
-    public static int[][] fillDiagonals() {
-        int size = 5;
-        int[][] array = new int[size][size];
-        for (int i = 0; i < size; i++) {
-            array[i][i] = 1;
-            array[i][size - 1 - i] = 1;
-        }
-        return array;
+    public static Employee[] createFiveEmployees() {
+        return new Employee[]{
+                new Employee("Иванов Иван Иванович", "тестировщик ПО",
+                        "ivan@company.ru", "+79993332211", 100000, 35),
+                new Employee("Петрова Анна Валерьевна", "тестировщик ПО",
+                        "anna@company.ru", "+79992345678", 120000, 42),
+                new Employee("Сидоров Андрей Сергеевич", "разработчик ПО",
+                        "andrey@company.ru", "+79992224566", 90000, 37),
+                new Employee("Петренко Дарья Васильевна", "разработчик ПО",
+                        "darya@company.ru", "+79997775544", 140000, 28),
+                new Employee("Максимов Максим Константинович", "аналитик",
+                        "maksim@company.ru", "+79992227788", 100000, 45),
+        };
     }
-
-    public static void printMatrix(int[][] matrix) {
-        for (int[] row : matrix) {
-            for (int element : row) {
-                System.out.print(element + "  ");
-            }
-            System.out.println();
-        }
-    }
-
-    public static int[] createArrayWithInitialValue(int len, int initialValue) {
-        int[] array = new int[len];
-        Arrays.fill(array, initialValue);
-        return array;
-    }
-
 }
 
