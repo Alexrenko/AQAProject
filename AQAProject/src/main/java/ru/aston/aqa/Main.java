80bea28f0730fcfbb8a73145313c859c23b3749c
Alex Renko <alexrenko@yahoo.com>
2023-11-28 19:28:45 +0100

    Lesson_7

--- AQAProject/src/main/java/ru/aston/aqa/Main.java
+++ AQAProject/src/main/java/ru/aston/aqa/Main.java
@@ -1,19 +1,46 @@
 package ru.aston.aqa;
 
+import ru.aston.aqa.exceptions.MyArrayDataException;
+import ru.aston.aqa.exceptions.MyArraySizeException;
+
 import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String[] header = {"value 1", "value 2", "value 3"};
-        int[][] data = {{100, 200, 123}, {300, 400, 500}};
+        String[][] correctArray = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"}
+        };
 
-        AppData appData = new AppData(header, data);
-        appData.save("/Volumes/Mac/Test/data.csv");
+        try {
+            int sum = sumArrayElements(correctArray);
+            System.out.println("Сумма элементов массива: " + sum);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            e.printStackTrace();
+        }
 
-        AppData loadedData = AppData.load("/Volumes/Mac/Test/data.csv");
+    }
 
+    public static int sumArrayElements(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (array.length != 4 || array[0].length != 4) {
+            throw new MyArraySizeException("Массив должен быть размером 4x4");
+        }
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j);
+                }
+            }
+        }
+        return sum;
     }
 
 }
---
+++ AQAProject/src/main/java/ru/aston/aqa/exceptions/MyArraySizeException.java
@@ -0,0 +1,8 @@
+package ru.aston.aqa.exceptions;
+
+public class MyArraySizeException extends Exception {
+
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
