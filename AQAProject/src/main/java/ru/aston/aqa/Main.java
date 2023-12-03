0fc022e31fdd7b2a6bfa6ec776f3b9997e809c30
Alex Renko <alexrenko@yahoo.com>
2023-12-03 15:38:59 +0100

    добавлен относительный путь к вайлу и хранение файла в каталоге ресуросов

--- AQAProject/src/main/java/ru/aston/aqa/Main.java
+++ AQAProject/src/main/java/ru/aston/aqa/Main.java
@@ -1,18 +1,22 @@
 package ru.aston.aqa;
 
 import java.awt.*;
+import java.io.File;
 
 public class Main {
 
     public static void main(String[] args) {
+        String filename = "data.csv";
+        String sep = File.separator;
+        String filePath = "src" + sep + "main" + sep + "resources" + sep + filename;
 
         String[] header = {"value 1", "value 2", "value 3"};
         int[][] data = {{100, 200, 123}, {300, 400, 500}};
-
         AppData appData = new AppData(header, data);
-        appData.save("/Volumes/Mac/Test/data.csv");
 
-        AppData loadedData = AppData.load("/Volumes/Mac/Test/data.csv");
+        appData.save(filePath);
+
+        AppData loadedData = AppData.load(filePath);
 
     }
 
