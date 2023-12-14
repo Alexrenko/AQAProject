f51e401831056c24a27a4d9dcd1915698f63f84f
Alex Renko <alexrenko@yahoo.com>
2023-12-14 17:40:31 +0100

    Lesson_12_junit_5

--- AQAProject/src/test/java/ru/aston/aqa/FactorialCalculatorTest.java
+++ AQAProject/src/test/java/ru/aston/aqa/FactorialCalculatorTest.java
@@ -1,46 +1,47 @@
 package ru.aston.aqa;
 
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FactorialCalculatorTest {
 
-    @Test(dataProvider = "positiveNumbers")
-    public void testFactorialPositive(int input, long expected) {
-        assertEquals(FactorialCalculator.factorial(input), expected);
-    }
-
-    @DataProvider(name = "positiveNumbers")
-    public Object[][] positiveNumbers() {
-        return new Object[][] {
-                {1, 1},
-                {2, 2},
-                {5, 120},
-                {10, 3628800}
-        };
+    @ParameterizedTest
+    @CsvSource({
+            "1, 1",
+            "2, 2",
+            "5, 120",
+            "10, 3628800"
+    })
+    void testFactorialPositive(int input, long expected) {
+        assertEquals(expected, FactorialCalculator.factorial(input));
     }
 
     @Test
-    public void testFactorialZero() {
-        assertEquals(FactorialCalculator.factorial(0), 1);
-    }
-
-    @Test(expectedExceptions = IllegalArgumentException.class)
-    public void testFactorialNegative() {
-        FactorialCalculator.factorial(-1);
+    void testFactorialZero() {
+        assertEquals(1, FactorialCalculator.factorial(0),
+                "Факториал нуля должен быть равен 1");
     }
 
     @Test
-    public void testFactorialLargeNumber() {
-        long result = FactorialCalculator.factorial(20);
-        assertTrue(result > 0);
+    void testFactorialNegative() {
+        assertThrows(IllegalArgumentException.class, () -> FactorialCalculator.factorial(-1),
+                "Факториал отрицательного числа должен вызывать исключение");
     }
 
-    @Test(expectedExceptions = ArithmeticException.class)
-    public void testFactorialOverflow() {
-        FactorialCalculator.factorial(200);
+    @Test
+    void testFactorialLargeNumber() {
+        assertDoesNotThrow(() -> FactorialCalculator.factorial(20),
+                "Факториал большого числа должен быть вычислен без вызова исключения");
+    }
+
+    @Test
+    void testFactorialOverflow() {
+        assertThrows(ArithmeticException.class, () -> FactorialCalculator.factorial(200),
+                "Факториал очень большого числа должен вызвать исключение из-за переполнения");
     }
 
 }
